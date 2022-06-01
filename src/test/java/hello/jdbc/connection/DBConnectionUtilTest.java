7269217e65b6e5d8ce7c1ca65d3f124d007002e7
yoonmin-kim <rladbsals23@gmail.com>
2022-06-01 20:28:19 +0900

    feat : jdbc 등록, 수정, 삭제, 테스트 (H2 DB)

---
+++ src/test/java/hello/jdbc/connection/DBConnectionUtilTest.java
@@ -0,0 +1,1 @@
+package hello.jdbc.connection;import static org.assertj.core.api.Assertions.*;import java.sql.Connection;import org.junit.jupiter.api.Test;import lombok.extern.slf4j.Slf4j;@Slf4jclass DBConnectionUtilTest {	@Test	void connection() {		Connection connection = DBConnectionUtil.getConnection();		assertThat(connection).isNotNull();	}}

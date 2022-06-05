8a53ee5f0eb3ff6c53429e02bf67fb925ca578b9
yoonmin-kim <rladbsals23@gmail.com>
2022-06-05 20:19:18 +0900

    feat : 스프링과 문제 해결 - 예외 처리, 반복
    하부 구현기술의 체크 익셉션을 모두 별도의 RuntimeException을 정의하여 익셉션 다시던지기를 하면
    이전에 체크 익셉션으로 인해 DIP, OCP가 깨지는 부분을 해결 할 수 있다.
    
    하지만 단순히 하나의 익셉션만 정의하여 사용하면 모르겠지만 상황에 따라서 익셉션을 catch하여
    복구처리를 하는 등의 부가적인 기능이 필요한 상황이 발생할 수 있다.
    
    그럴경우 에러코드, 익셉션상태 등을 확인하여 처리를 해줘야하는데 그런 상황에 맞게 모든 익셉션을
    재정의 하기에는 개발자의 리소스가 너무 낭비된다. 뿐만 아니라 단순히 MySQL, Oracle, H2 등
    데이터베이스 종류만 달라져도 에러코드, 상태등이 다 다르다.
    
    이러한 부분을 스프링은 추상화하여 스프링 전용 Exception으로 제공해주며 Exception변환처리도
    알아서 해주기 때문에 개발자는 스프링의 코드에만 의존성을 갖게하여 위와 같은 문제를 모두 해결할 수 있다
    
    추가로 순수jdbc코드(try-catch, close)의 모든 작업은 스프링에서 제공하는 jdbcTemplate으로 한번에 해결할 수 있다

---
+++ src/main/java/hello/jdbc/repository/MemberRepository.java
@@ -0,0 +1,1 @@
+package hello.jdbc.repository;import java.sql.SQLException;import hello.jdbc.domain.Member;public interface MemberRepository {	Member save(Member member);	Member findById(String memberId);	void update(String memberId, int money);	void delete(String memberId);}
